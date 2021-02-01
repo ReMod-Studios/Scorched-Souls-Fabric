8b3daeb5246716835877c670f5a5170a316bc7ed
LePichu <ishat.prsnl65844@gmail.com>
2021-02-01 12:20:43 +0530

    initial

---
+++ src/main/java/com/remodstudios/scouls/ScorchedSouls.java
@@ -0,0 +1,26 @@
+package com.remodstudios.scouls;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ScorchedSouls implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "scouls";
+    public static final String MOD_NAME = "Scorched Souls";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
